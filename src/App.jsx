f543aa2ba5fdc3924517f03ea943e1757dfbf2d2
Emanuel Peire <emapeire@outlook.com>
2023-11-14 14:39:04 -0300

    Add form styling and structure to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 
 export default function App() {
   return (
-    <>
-      <h1>React Movie Search</h1>
-      <form className='form'>
-        <label>Put yout movie here:</label>
-        <input type='text' placeholder='The Matrix...' />
-        <button type='sumbit'>Search</button>
-      </form>
-    </>
+    <div className='page'>
+      <header>
+        <h1>React Movie Search</h1>
+        <form className='form'>
+          <label className='label'>Put yout movie here:</label>
+          <input type='text' placeholder='The Matrix...' />
+          <button type='sumbit'>Search</button>
+        </form>
+      </header>
+      <main>Here the results...</main>
+    </div>
   )
 }
