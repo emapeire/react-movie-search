03c95b1eb65851523db7e653333c24e38f8a9dee
Emanuel Peire <emapeire@outlook.com>
2023-11-14 13:44:52 -0300

    Add movie search form to React app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ function App() {
   return (
     <>
       <h1>React Movie Search</h1>
+      <form>
+        <label>Put yout movie here:</label>
+        <input type='text' placeholder='Search...' />
+      </form>
+      <button>Search</button>
     </>
   )
 }
