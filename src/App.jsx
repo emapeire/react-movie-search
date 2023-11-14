e962e986d715fca2e753185d9d21e80ea1708803
Emanuel Peire <emapeire@outlook.com>
2023-11-14 15:06:43 -0300

    Add movie search functionality to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/without-results.json'
 
 export default function App() {
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
+
   return (
     <div className='page'>
       <header>
         <h1>React Movie Search</h1>
         <form className='form'>
           <label className='label'>Put yout movie here:</label>
-          <input type='text' placeholder='The Matrix...' />
+          <input type='text' placeholder='Matrix...' />
           <button type='sumbit'>Search</button>
         </form>
       </header>
-      <main>Here the results...</main>
+      <main>
+        {hasMovies ? (
+          <il>
+            {movies.map((movie) => (
+              <lu key={movie.imdbID}>
+                <img src={movie.Poster} alt={movie.Title} />
+                <h2>{movie.Title}</h2>
+                <p>{movie.Year}</p>
+              </lu>
+            ))}
+          </il>
+        ) : (
+          <p>No movies found</p>
+        )}
+      </main>
     </div>
   )
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const API_MOVIE_URL = 'http://www.omdbapi.com/?apikey=4287ad07&s='
