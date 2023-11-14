1251d79169f8bea8f25f3e0d74f1213cb764ba57
Emanuel Peire <emapeire@outlook.com>
2023-11-14 13:52:40 -0300

    Refactor code to improve performance and readability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css'
 
-function App() {
+export default function App() {
   return (
     <>
       <h1>React Movie Search</h1>
-      <form>
+      <form className='form'>
         <label>Put yout movie here:</label>
         <input type='text' placeholder='The Matrix...' />
+        <button type='sumbit'>Search</button>
       </form>
-      <button>Search</button>
     </>
   )
 }
-
-export default App
